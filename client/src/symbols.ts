7963d70e9f0fb8e815de1860097c47e7f65a2ed0
Senryoku <maretverdant@gmail.com>
2022-11-25 02:06:27 +0100

    .

--- client/src/symbols.ts
+++ client/src/symbols.ts
@@ -1,12 +1,16 @@
 export const mana_symbols = {};
 import mana_symbols_list from "./assets/data/symbology.json";
+
+function getImage(svg_uri) {
+	return new URL(`/src/assets/img/symbols/${svg_uri}`, import.meta.url).href;
+}
+
 for (let symbol of mana_symbols_list) {
 	mana_symbols[symbol.symbol] = symbol;
 
-	mana_symbols[symbol.symbol].svg_uri = new URL(
-		"/src/assets/img/symbols/" + mana_symbols[symbol.symbol].svg_uri,
-		import.meta.url
-	).href;
+	mana_symbols[symbol.symbol].svg_uri = getImage(
+		mana_symbols[symbol.symbol].svg_uri
+	);
 }
 
 export default mana_symbols;
